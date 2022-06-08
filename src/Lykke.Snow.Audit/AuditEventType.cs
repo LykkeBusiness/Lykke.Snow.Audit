1031ffdbf5934d2e4a82115b04d926ffaf0291bc
atarutin <andrey.tarutin@lykke-business.ch>
2022-06-08 17:29:56 +0300

    added AuditEventType.cs

---
+++ src/Lykke.Snow.Audit/AuditEventType.cs
@@ -0,0 +1,12 @@
+namespace Lykke.Snow.Audit
+{
+    /// <summary>
+    /// An audit event type
+    /// </summary>
+    public enum AuditEventType
+    {
+        Edition,
+        Creation,
+        Deletion
+    }
+}
